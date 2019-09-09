4b79967a9c530885df7c16cedac78a68c5cb6d88
srius1977 <minameissri@gmail.com>
2019-09-09 19:07:43 -0400

    Add project files.

---
+++ temp_Console1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace temp_Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
